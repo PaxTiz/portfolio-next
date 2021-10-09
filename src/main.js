6b26dade7637cf72f765a9fe5970a5a71988c94d
Valentin Cernuta <vcernuta3@gmail.com>
2021-10-10 00:37:09 +0200

    Add authentication to protect admin page

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './assets/css/app.css'
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+import VueCookies from 'vue3-cookies'
+
+const app = createApp(App)
+app.use(router)
+app.use(VueCookies)
+app.mount('#app')
+
+app.provide('cookies', app.config.globalProperties.$cookies)
+
+router.app = app
--- src/router.js
+++ src/router.js
@@ -7,7 +7,14 @@ const routes = [
     { path: '/works', name: 'works', component: () => import('@/pages/Works.vue') },
     { path: '/contact', name: 'contact', component: () => import('@/pages/Contact.vue') },
     { path: '/login', name: 'login', component: () => import('@/pages/admin/Login.vue') },
-    { path: '/admin', name: 'admin', component: () => import('@/pages/admin/Admin.vue') },
+    { 
+        path: '/admin', 
+        name: 'admin', 
+        meta: {
+            requiresAuth: true
+        },
+        component: () => import('@/pages/admin/Admin.vue') 
+    },
 ]
 
 const router = createRouter({
@@ -15,4 +22,17 @@ const router = createRouter({
     routes,
 })
 
-  export default router
+router.beforeEach((to, from, next) => {
+    const { $cookies } = router.app.config.globalProperties
+    const isAuth = $cookies.get('user') && $cookies.get('access_token') && $cookies.get('refresh_token')
+    if (to.name === 'login' && isAuth) {
+        return next({ name: 'admin' })
+    }
+    if (to.meta.requiresAuth && !isAuth) {
+        return next({ name: 'login' })
+    }
+
+    return next()
+})
+
+export default router
